d99086d0f1bfa41295bb1442f649050b9cccf46a
Jenner <Jenner@DESKTOP-9MOV8TJ.home>
2021-03-23 11:43:26 +0100

    Scripts2020

--- Scripts-users/01a-funcions.sh
+++ Scripts-users/01a-funcions.sh
@@ -1,35 +1,33 @@
 #! /bin/bash
-# Exemples de funcions
+# @JennerSarmiento ISX 46994723
+# Ejemplos de funciones
+#-----------------------------------------------
 
-#Validar si rep un argument
-#Validar existeix user
-#mostrar camp per camp
-
-ERROR_NARGS=1
-ERROR_USER=2
-#1 Validar argumentos
-
-if [ $# -ne 1 ]
+function showuser(){
+if [ $# -eq 0 ]
 then
-  echo "Error:Numero de arguments erroni"
-  echo "USAGE:program.sh arg1"
-  exit $ERROR_NARGS
+	echo "Numero de argumentos no valido"
+	return 1
 fi
-
-#2 Validar l'existencia de user
 user=$1
-
-grep "^$user:" /etc/passwd &> /dev/null
+linea=$(echo $user| grep "^$user:" /etc/passwd)
 if [ $? -eq 0 ]
-then
-  echo $user
+then 
+    login=$(echo $linea | cut -d: -f1)
+    echo "LOGIN--> $login"
+    uid=$(echo $linea | cut -d: -f3)
+    echo "UID--> $uid"
+    gid=$(echo $linea | cut -d: -f4)
+    echo "GID--> $gid"
+    gecos=$(echo $linea |cut -d: -f5)
+    echo "GECOS--> $gecos"
+    homedir=$(echo $linea | cut -d: -f6)
+    echo "HOME_DIR--> $homedir"
+    bash=$(echo $linea | cut -d: -f7)
+    echo "BASH--> $bash"
 else
-  echo "ERROR: El usuario $user no existeix al sistema"
-  exit $ERROR_USER
+       echo "ERROR el usuario $1 no existe"
+       return 0       
 fi
-exit 0
-#3 Mostrar camp per camp
-
-function_user(){
- 
-} 
+return 0
+}
